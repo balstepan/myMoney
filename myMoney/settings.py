9e91a4a5779f973aef9dee2115a985b809d1ae0f
Stepan B <tudum@bk.ru>
2020-11-17 16:02:58 +0300

    Add password reset

--- myMoney/settings.py
+++ myMoney/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
